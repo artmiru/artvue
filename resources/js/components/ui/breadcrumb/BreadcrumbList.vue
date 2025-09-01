d4d870b71c4f76911cba609418321c0d03ed3d21
Evsei <artmiru@gmail.com>
2025-09-01 17:49:08 +0300

    22

--- resources/js/components/ui/breadcrumb/BreadcrumbList.vue
+++ resources/js/components/ui/breadcrumb/BreadcrumbList.vue
@@ -10,7 +10,7 @@ const props = defineProps<{
 <template>
   <ol
     data-slot="breadcrumb-list"
-    :class="cn('text-muted-foreground flex flex-wrap items-center gap-1.5 text-sm break-words gap-2.5 bg-neutral-50 px-3 text-xs rounded-b', props.class)"
+    :class="cn('text-muted-foreground flex flex-wrap items-center gap-1.5 break-words bg-neutral-50 px-3 text-xs rounded-b', props.class)"
   >
     <slot />
   </ol>
